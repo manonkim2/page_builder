b99522d3afdf0c10fed82a229cc8c0649c7606d2
Manon Kim <kim.manon2@gmail.com>
2024-07-16 12:44:32 +0900

    [feat] Toast component

---
+++ services/storybook/stories/ReactComponents/Layout/Toast.stories.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "@manon/react-components-button/style.css";
+import { Button } from "@manon/react-components-button";
+
+import { ToastProvider, useToast } from "@manon/react-components-toast";
+import "@manon/react-components-toast/style.css";
+
+export default {
+  title: "React Components/Toast",
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+const Example = () => {
+  const { toast } = useToast();
+
+  return (
+    <Button
+      onClick={() =>
+        toast({
+          payload: {
+            message: "Hello, World!",
+          },
+        })
+      }
+    >
+      Toast Button
+    </Button>
+  );
+};
+
+export const ToastStory = {
+  render: () => (
+    <ToastProvider>
+      <Example />
+    </ToastProvider>
+  ),
+};
