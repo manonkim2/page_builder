6d94f9573188098d149c4926f6d428e3666a3f6b
Manon Kim <kim.manon2@gmail.com>
2024-07-19 19:42:29 +0900

    [chore] setting

---
+++ services/page_builder/src/pages/playground.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@manon/react-components-layout";
+import { vars } from "@manon/themes";
+
+const PlaygroundPage = () => {
+  return (
+    <Box padding={3}>
+      <h1>{vars.colors.$scale.gray[900]}</h1>
+    </Box>
+  );
+};
+
+export default PlaygroundPage;
